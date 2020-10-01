71ed02fe7b8664aec77e767ebc7890975b8246e1
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-10-01 12:49:32 +0300

    Update ft_percent_lower_uppercase.py

--- ft_percent_lower_uppercase.py
+++ ft_percent_lower_uppercase.py
@@ -1,9 +1,18 @@
-def ft_percent_lower_uppercase(str):
-    up = 0
-    low = 0
-    for i in str:
-        if i == i.upper():
-            up += 1
-        else:
-            low += 1
-    return up / low * 100
+def ft_percent_lower_uppercase(x):
+    a = 'qwertyuioplkjhgfdsazxcvbnm'
+    A = 'QWERTYUIOPLKJHGFDSAZXCVBNM'
+    b = 'ёйцукенгшщзхъэждлорпавыфячсмитьбю'
+    B = 'ЁЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ'
+    s = 0
+    h = 0
+    c = 0
+    for i in x:
+        if i in a or i in b:
+            s += 1
+        if i in A or i in B:
+            h += 1
+        c += 1
+    if s / h == 1:
+        return 1
+    else:
+        return s / h * 100
