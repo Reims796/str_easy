09477796901e05faa826b14c1d11942a90073df6
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-10-01 12:15:26 +0300

    Update ft_even_place.py

--- ft_even_place.py
+++ ft_even_place.py
@@ -1,14 +1,14 @@
-def ft_len(str):
-    a = 0
-    for i in str:
-        a += 1
-    return a
+def ft_len(a):
+    b = 0
+    for i in a:
+        b += 1
+
+    return b
 
 
-def ft_even_place(str):
-    x = ""
-    b = ft_len(str)
-    for i in range(b):
-        if i % 2 == 0:
-            x = x + str[i]
+def ft_even_place(a):
+    x = str()
+    for i in range(ft_len(a)):
+        if i % 2 == 1:
+            x += a[i]
     return x
