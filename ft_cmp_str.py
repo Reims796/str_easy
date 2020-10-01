1fbbe799c118a6313bbcb879595819d0633a37d2
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-10-01 21:26:25 +0300

    Update ft_cmp_str.py

--- ft_cmp_str.py
+++ ft_cmp_str.py
@@ -1,9 +1,17 @@
-def ft_cmp_str(str1, str2, num):
-    result = ""
-    for i in range(num):
-        result = result + str1[i]
-    result = result + str2
+def ft_len(str):
+    a = 0
+    for i in str:
+        a += 1
+    return (a)
 
-    for i in range(num, len(str1)):
-        result = result + str1[i]
-    return result
+
+def ft_cmp_str(str1, str2, num):
+    x = ""
+    num = num - 1
+    for i in range(num):
+        x = x + str1[i]
+    x = x + str2
+
+    for i in range(num, ft_len(str1)):
+        x = x + str1[i]
+    return x
