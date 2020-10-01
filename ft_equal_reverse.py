0c5de6d95904ac951690bce5fe0c8cd40f40b781
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-10-01 12:58:46 +0300

    Update ft_equal_reverse.py

--- ft_equal_reverse.py
+++ ft_equal_reverse.py
@@ -1,6 +1,19 @@
-def ft_equal_reverse(str):
-    if str == str[::-1]:
-        return True
-    else:
-        return False
+def ft_len(str):
+    a = 0
+    for i in str:
+        a += 1
+    return a
 
+
+def ft_reverse_str(str):
+    b = ""
+    for i in range(ft_len(str) - 1, -1, -1):
+        b += str[i]
+    return b
+
+
+def ft_equal_reverse(str):
+    c = ft_reverse_str(str)
+    if c == str:
+        return True
+    return False
